157490338c35cdb6b2eeaeb7b752d57115020c1a
KrzysztofBialik <105109608+KrzysztofBialik@users.noreply.github.com>
2023-06-10 12:43:49 +0200

    Crate Interactions

--- Assets/Scripts/ObjectInteractions.cs
+++ Assets/Scripts/ObjectInteractions.cs
@@ -5,25 +5,45 @@ using UnityEngine;
 public class ObjectInteractions : MonoBehaviour
 {
 
-    [SerializeField] private Transform player;
-    [SerializeField] private Rigidbody2D rb;
+    // [SerializeField] private Transform player;
+    private Rigidbody2D rb;
+    [SerializeField] private GameObject crateInfo;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
 
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("KnightInteract"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2"))
         {
             Debug.Log("Collision");
-            if (player.gameObject.name != "Magician")
+            if (collision.gameObject.name != "Magician")
             {
+                if(crateInfo != null)
+                {
+                    crateInfo.SetActive(false);
+                }
                 Debug.Log("Ddd");
                 rb.bodyType = RigidbodyType2D.Dynamic;
             }
             else
             {
                 rb.bodyType = RigidbodyType2D.Static;
+            }
+        }
+    }
 
+    private void OnCollisionExit2S(Collision2D collision) 
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2"))
+        {
+            if (collision.gameObject.name != "Magician")
+            {
+                rb.bodyType = RigidbodyType2D.Static;
             }
         }
     }
